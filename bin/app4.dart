5f1161dda54014cbdc56eb2098d5be5959a160e8
abhigyan <abhigyan709@gmail.com>
2021-05-09 18:47:00 +0530

    "Required parameters in Flutter Case"

--- bin/app4.dart
+++ bin/app4.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// Optional Parameters
 /// In dart function parameters can be optional in the sense that you don't
 /// provide them, the compiler will assign null or a default value you have
@@ -20,6 +22,12 @@ void test2({int? c, int? d = 0}) {
 void test3({int e = 0, required int f}) {
   print("Value of e is $e");
   print("Value of f is $f"); // throw an error if the value is not given on call
+}
+
+// most used case in flutter for required parameter
+void test4(int g, {int h = 0}) {
+  print("Value of g $g"); // required
+  print("Value of h $h");
 }
 
 void main() {
@@ -45,4 +53,7 @@ void main() {
   // already throwing an eception test3(e: 0);
   test3(f: 6);
   test3(e: 5, f: 98);
+
+  test4(6); // if not given throw the error
+  test4(6, h: 100);
 }
