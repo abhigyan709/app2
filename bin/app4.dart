814f089d5b8575fa5ea6c061e0e8ba68650ba6eb
abhigyan <abhigyan709@gmail.com>
2021-05-09 18:12:48 +0530

    "Dafault values to Function"

--- bin/app4.dart
+++ bin/app4.dart
@@ -9,6 +9,24 @@ void test({int a, int b}) {
   print("Value of b: $b");
 }
 
+void test2({int c, int d = 0}) {
+  print("Value of c: $c");
+  print("Value of d: $d");
+  // default value of the is assigned to zero
+}
+
 void main() {
   test(a: 2, b: -6);
+  // the position doesent matter in named parameters
+  test(b: -6, a: 5);
+
+  // when a parameter is missing the default null value is given;
+
+  test(a: -8); // value of will be printed of null
+
+  // when the dafault value is set to optional parameter
+  test2(c: 5); // value of d is set to zero therefore it will print zero
+
+  // when the value is given during functional call
+  test2(c: 9, d: 10); // value of the d will be 10
 }
