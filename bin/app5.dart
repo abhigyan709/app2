ced3ab171389df623df5fb448838b615ba6e84d2
abhigyan <abhigyan709@gmail.com>
2021-05-09 22:13:26 +0530

    "Nested function Done"

--- bin/app5.dart
+++ bin/app5.dart
@@ -1,1 +1,25 @@
+import 'dart:math';
+
 /// working upon nested functions
+// function under the function is known as nested functions
+void testInner(int value) {
+  // Nested function
+  int randomValue() => Random().nextInt(10);
+
+  // using the nested function
+  final number = value + randomValue();
+  print("$number");
+}
+
+void main() {
+  testInner(20);
+  // you cant use the randomValue() function in this.
+}
+
+/// good practices and bad practices
+/// void test([int a = 0]){...} // good
+/// void test([int a : 0]){...} // bad
+/// the colon initiaization might be removed in the future
+/// you should never initialize nullables with null beacuse the compiler already
+/// does by default
+///
